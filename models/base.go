cb4b845b44967354c39edbab2efa519beeac7c3f
Chris Wray <chriswray76@gmail.com>
2019-11-21 10:51:06 +0000

    hardcoded db url

--- models/base.go
+++ models/base.go
@@ -25,7 +25,7 @@ func init() {
 	dbUri := fmt.Sprintf("host=%s dbname=%s sslmode=disable", dbHost, dbName)
 	fmt.Println(dbUri)
 
-	conn, err := gorm.Open("postgres", dbUri)
+	conn, err := gorm.Open("postgres", "postgres://zptmemqneaixmt:1e92c2cba5790b83e52506765ba9fd47ec83cf5d41a53db4420aac6a71a380c4@ec2-23-23-241-119.compute-1.amazonaws.com:5432/datl5hlol38lth")
 	if err != nil {
 		fmt.Print(err)
 	}
